c3b931bd9b7ce23bd118631f7d5b1e8f08e100fb
katyvonkaty <kj0203@gmail.com>
2020-10-05 10:20:17 -0400

    updated

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,35 +1,34 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {spans: 0}
-this.imageRef = React.createRef();
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
   }
 
-componentDidMount(){
-  this.imageRef.current.addEventListener("load", this.setSpans);
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
   }
 
-  setSpans =() => {
-  const height =this.imageRef.current.clientHeight
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
 
-  const spans = Math.ceil (height / 20)
-  this.setState({spans})
-}
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
 
   render() {
+    const { description, urls } = this.props.image;
 
-    const {urls, description} = this.props.image;
-      return (
-        <div style= {{ gridRowEnd: `span ${this.state.spans}`}}>
-          <img
-          ref={this.imageRef}
-          src={urls.regular}
-          alt={description} />
-        </div>
-      )
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
   }
 }
 
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,13 @@
-import "./ImageList.css";
+import './ImageList.css';
 import React from 'react';
-import ImageCard from "./ImageCard"
+import ImageCard from './ImageCard';
 
-const ImageList = (props) => {
+const ImageList = props => {
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
+  });
 
-  const images = props.images.map( (image)=>{
-    return <ImageCard image= {image}
-      key={image.id}
-       />
-  })
-  return <div className ="image-list"> {images} </div>
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
